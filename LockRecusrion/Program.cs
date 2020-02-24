b6d20230c1a58881752cd6f13d3668200046a3c2
Sumit Jana <sumitjana92@hotmail.com>
2020-02-25 02:47:48 +0530

    Add project files.

---
+++ LockRecusrion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LockRecusrion
+{
+    class Program
+    {
+        static SpinLock sl = new SpinLock();
+        static void Main(string[] args)
+        {
+            LockRecusrion(5);
+            Console.ReadKey();
+        }
+        static void LockRecusrion(int x)
+        {
+            bool lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+            }
+            catch (LockRecursionException e)
+            {
+                Console.WriteLine("exception " + e);
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Console.WriteLine($"Took a lock, x= {x}");
+                    LockRecusrion(x-1);
+                    sl.Exit();
+                }
+                else
+                {
+                    Console.WriteLine($"failed to take a lock for x={x}");
+                }
+            }
+        }
+    }
+}
---
+++ SpinLockingAndLockRecursion/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpinLockingAndLockRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+            SpinLock sl = new SpinLock();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(()=> {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        var lockTaken = false;
+                        try
+                        {
+                            sl.Enter(ref lockTaken);
+                            ba.Deposit(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken) 
+                                sl.Exit();
+                            
+                        }
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() => {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        var lockTaken = false;
+                        try
+                        {
+                            sl.Enter(ref lockTaken);
+                            ba.Withdraw(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken)
+                                sl.Exit();
+                        }
+                    }
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine(ba.Balance);
+            Console.ReadKey();
+        }
+    }
+    class BankAccount
+    {
+        public int Balance { get; set; }
+        public void Deposit(int amt)
+        {
+            Balance += amt;
+        }
+        public void Withdraw(int amt)
+        {
+            Balance -= amt;
+        }
+    }
+}
